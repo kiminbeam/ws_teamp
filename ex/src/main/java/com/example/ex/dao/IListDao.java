8902f14b81ed66343c58f0fac060f9783a998354
sunny-window <sunnywindow0203@gmail.com>
2024-05-01 12:45:06 +0900

    Dao 와 Service 연결

--- ex/src/main/java/com/example/ex/dao/IListDao.java
+++ ex/src/main/java/com/example/ex/dao/IListDao.java
@@ -2,9 +2,11 @@ package com.example.ex.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.example.ex.dto.ListDto;
 
-
+@Service
 public interface IListDao {
 	
 	public List<ListDto> getList();
